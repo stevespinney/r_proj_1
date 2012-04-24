c16b4e614f078cb35812c3530ab7eb8be5ab9582
Steve Spinney <sspinney@heritagecenters.org>
2012-04-24 12:32:54 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acetis::Application.config.secret_token = '739cbe1d70f94ea10719a55fd1f61c665315239c17bb3e5a8fc0d1224a70f2d67538abee536808dcc4dacfd0d5aa86e4c3f8e4326352bd81b48f70047fecc705'
